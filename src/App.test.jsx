e2a20ca250e58fefc566fdc7dddf10314de2a524
Grant Kiely <grant.kiely@gmail.com>
2022-06-24 11:38:33 -0500

    Add jsdom

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,13 @@ import { describe, it, expect } from 'bun:test';
 import React from 'react'
 import { render } from '@testing-library/react';
 
+// Enable jsdom to render the DOM
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const dom = new JSDOM('<!doctype html><html><body></body></html>');
+global.window = dom.window;
+global.document = dom.document;
+
 import App from './App';
 
 describe('test', () => {
