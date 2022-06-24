31063da2c5c81433280914c45dccc8605dd191cb
Grant Kiely <grant.kiely@gmail.com>
2022-06-24 11:41:35 -0500

    Added examples for different dom libraries

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,12 +2,23 @@ import { describe, it, expect } from 'bun:test';
 import React from 'react'
 import { render } from '@testing-library/react';
 
-// Enable jsdom to render the DOM
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const dom = new JSDOM('<!doctype html><html><body></body></html>');
-global.window = dom.window;
-global.document = dom.document;
+// jsdom
+// import { jsdom } from 'jsdom';
+// const { JSDOM } = jsdom;
+// const dom = new JSDOM('<!doctype html><html><body></body></html>');
+
+// happy-dom
+// import { Window } from 'happy-dom';
+// const window = new Window();
+// const document = window.document;
+
+// linkedom
+// const { parseHTML } = require('linkedom');
+// const dom = parseHTML(`<!doctype html><html lang="en"><head /><body /></html>`);
+
+// Enable dom
+// global.window = dom.window;
+// global.document = dom.document;
 
 import App from './App';
 
